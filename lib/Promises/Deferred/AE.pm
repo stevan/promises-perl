134ffc0df96ef1be363022617a53212825e32532
Stevan Little <stevan.little@iinteractive.com>
2013-12-30 12:17:13 -0500

    adding some docs

--- lib/Promises/Deferred/AE.pm
+++ lib/Promises/Deferred/AE.pm
@@ -1,10 +1,12 @@
 package Promises::Deferred::AE;
+# ABSTRACT: An implementation of Promises in Perl
 
 use strict;
 use warnings;
+
 use AE;
-use Promises::Deferred;
-our @ISA = qw(Promises::Deferred);
+
+use parent 'Promises::Deferred';
 
 sub _notify {
     my ( $self, $callbacks, $result ) = @_;
@@ -17,3 +19,21 @@ sub _notify {
 }
 
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    use Promises backend => ['AE'], qw[ deferred collect ];
+
+    # ... everything else is the same
+
+=head1 DESCRIPTION
+
+The "Promise/A+" spec strongly suggests that the callbacks
+given to C<then> should be run asynchronously (meaning in the
+next turn of the event loop), this module provides support for
+doing so using the L<AE> module.
+
+=back
+

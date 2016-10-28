2e4c3acdc4548d463882175f73041b54ab4e4168
stuckdownawell <stuckdownawell@gmail.com>
2016-10-28 08:39:34 +0100

    Change to workaround seen in 5.24.0
    Issue where a moose object is thrown and it has meta->superclasses called on it.  The do { $error = $@ || .... } results in $error = undef

--- lib/Promises/Deferred.pm
+++ lib/Promises/Deferred.pm
@@ -137,7 +137,7 @@ sub _wrap {
             @results = do { $f->(@_) };
             1;
         }
-            || do { $error = $@ || 'Unknown reason' };
+            || do { $error = $@ // 'Unknown reason' };
 
         if ($error) {
             $d->reject($error);
